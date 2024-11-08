27a86e951b8698831f6d3bd8846344c4818ab969
DoTienOanh <oanhvy2bg@gmail.com>
2024-11-09 01:24:02 +0700

    update rooms

--- rooms.php
+++ rooms.php
@@ -191,5 +191,8 @@
     </div>
    </div>
    <?php require('inc/footer.php'); ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+  
 </body>
 </html>
